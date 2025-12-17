d8a50d5363dfede3c387f16135367ee022ed93f1
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-12-17 23:34:41 +0530

    Function with Event Emitter Pattern

--- 02-functions/app.js
+++ 02-functions/app.js
@@ -532,6 +532,28 @@ console.log(pureAdd(3, 4)); // Output: 7
 console.log(pureAdd(3, 4)); // Output: 7
 console.log(pureAdd(5, 6)); // Output: 11
     
-
+//  Function with Event Emitter Pattern
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+    on(event, listener) {
+        if (!this.events[event]) {
+            this.events[event] = [];
+        }
+        this.events[event].push(listener);
+    }
+    emit(event, ...args) {
+        if (this.events[event]) {
+            this.events[event].forEach(listener => listener(...args));
+        }
+    }
+}
+const emitter = new EventEmitter();
+emitter.on("greet", (name) => {
+    console.log(`Hello, ${name}!`);
+});
+emitter.emit("greet", "Frank"); // Output: Hello, Frank!
+    
 
 
