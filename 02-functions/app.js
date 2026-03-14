5071bf614efd35d6b2dd5edc1ef21ebd4ab9e8b0
Shivaprada Upadya <shivaprada2209@gmail.com>
2026-03-14 23:09:55 +0530

    Function with Error Handling in Async/Await

--- 02-functions/app.js
+++ 02-functions/app.js
@@ -2022,6 +2022,23 @@ fetchWithErrorHandling("https://jsonplaceholder.typicode.com/posts/1"); // Valid
 fetchWithErrorHandling("https://jsonplaceholder.typicode.com/invalid-url"); // Invalid URL
 // Output: Fetched data: { userId: 1, id: 1, title: '...', body: '...' }
 
+//  Function with Error Handling in Async/Await
+async function fetchDataAsyncWithErrorHandling(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log("Fetched data:", data);
+    } catch (error) {
+        console.error("Caught error in async fetch:", error.message);
+    }
+}
+fetchDataAsyncWithErrorHandling("https://jsonplaceholder.typicode.com/posts/1"); // Valid URL
+fetchDataAsyncWithErrorHandling("https://jsonplaceholder.typicode.com/invalid-url"); // Invalid URL
+// Output: Fetched data: { userId: 1, id: 1, title: '...', body: '...' }
+
 
 
 
