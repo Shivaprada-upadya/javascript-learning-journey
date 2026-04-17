3381d565f93fde611ebb6fb8a13ac4121634f948
Shivaprada Upadya <shivaprada2209@gmail.com>
2026-04-17 23:56:26 +0530

    Function with Error Handling in Timers

--- 02-functions/app.js
+++ 02-functions/app.js
@@ -2557,6 +2557,18 @@ async function* asyncGeneratorWithErrorHandling() {
     console.log(await asyncGen.next()); // Output: { value: undefined, done: true }
 })();
 
+//  Function with Error Handling in Timers
+function timerWithErrorHandling() {
+    setTimeout(() => {
+        try {
+            throw new Error("Error in timer.");
+        } catch (error) {
+            console.error("Caught error in timer:", error.message);
+        }
+    }, 1000);
+}
+timerWithErrorHandling();
+// Output after 1 second: Caught error in timer: Error in timer.
 
 
 
