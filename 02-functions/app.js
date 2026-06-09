5c2d72529877fc164f6567410db4406ff1a25dc8
Shivaprada Upadya <shivaprada2209@gmail.com>
2026-06-09 21:50:54 +0530

    Function with Logging and Service Workers

--- 02-functions/app.js
+++ 02-functions/app.js
@@ -3436,6 +3436,25 @@ function loggedWebWorker() {
 }
 loggedWebWorker();
     
+//  Function with Logging and Service Workers
+function loggedServiceWorker() {
+    if ('serviceWorker' in navigator) {
+        console.log("Registering service worker...");
+        navigator.serviceWorker.register('service-worker.js')
+            .then(registration => {
+                console.log("Service worker registered with scope:", registration.scope);
+            })
+            .catch(error => {
+                console.error("Caught error in service worker registration:", error.message);
+            }
+        );
+    } else {
+        console.log("Service workers are not supported in this browser.");
+    }
+}
+loggedServiceWorker();
+
+
 
 
 
